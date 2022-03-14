533552748a573512765f114060260d3262edf619
Agmaromatic <etu_agmar.abdeslamyassine@emsi-edu.ma>
2022-03-14 15:35:34 +0100

    bark

--- src/com/sqli/Main.java
+++ src/com/sqli/Main.java
@@ -3,10 +3,12 @@ package com.sqli;
 public class Main {
 
 
-
+public static String dog(){
+        return "bark";
+}
     public static void main(String[] args) {
 	// write your code here
-
+    String mydog=dog();
 
     }
 }
