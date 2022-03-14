97cb3d13b6ad8cec1abf8f2eaba15c7a6cac45d4
Agmaromatic <etu_agmar.abdeslamyassine@emsi-edu.ma>
2022-03-14 15:34:31 +0100

    add cat fnc

--- src/com/sqli/Main.java
+++ src/com/sqli/Main.java
@@ -2,11 +2,13 @@ package com.sqli;
 
 public class Main {
 
-
+    public static String cat(){
+        return "meow";
+    }
 
     public static void main(String[] args) {
 	// write your code here
-
+        String mycat=cat();
 
     }
 }
